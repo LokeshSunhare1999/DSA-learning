2ffeeebddf121813784091e32a98e885fc21090b
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2024-01-17 22:21:59 +0530

    if else and switch switch statement

---
+++ else_if_else_if.js
@@ -0,0 +1,11 @@
+let place = "second";
+
+if (place == "first") {
+  console.log("Gold");
+} else if (place == "second") {
+  console.log("Silver");
+} else if (place == "third") {
+  console.log("Bronze");
+} else {
+  console.log("No medal");
+}
