0152028a84dcea822c1b4c79f4583a60cb927da3
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2024-02-01 23:48:08 +0530

    arrrat_obj_exercise

---
+++ array_object_exercise.js
@@ -0,0 +1,19 @@
+const clothes = [];
+clothes.push("Jeans");
+clothes.push("Pant");
+clothes.push("Shirt");
+clothes.push("Cap");
+clothes.push("T-shirt");
+console.log("clothes-1", clothes);
+clothes.pop("T-shirt");
+console.log("clothes-2", clothes);
+clothes.push("HOODY");
+console.log("clothes-3", clothes);
+console.log("clothes-3", clothes[2]);
+
+const favCar = {};
+
+favCar.color = "black";
+favCar.convertible = true;
+
+console.log("favCar", favCar);
