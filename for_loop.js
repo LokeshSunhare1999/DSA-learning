807c2c91b469199a3893641c3df14e5320fe7608
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-05-26 23:34:32 +0530

    remove prev. files

--- for_loop.js
+++
@@ -1,9 +0,0 @@
-for (let i = 1; i <= 5; i++) {
-  console.log(i);
-}
-console.log("Counting completed!");
-
-for (let i = 5; i >= 1; i--) {
-  console.log(i);
-}
-console.log("Countdown finished!");
--- if_else.js
+++
@@ -1,7 +0,0 @@
-let result = 50;
-
-if (result > 40) {
-  console.log("You passed the text");
-} else {
-  console.log("You did not pass the test");
-}
