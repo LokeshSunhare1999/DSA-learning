718fcf2227ebf80d7aab0953375b34c0d1e6b9e2
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-05-27 23:48:27 +0530

    insertion sort

---
+++ Step-2/sorting-algorithm/Insertion-sort.js
@@ -0,0 +1,16 @@
+let arr = [133, 46, 24, 52, 20, 9];
+let j;
+function arrSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    j = i;
+    while (j > 0 && arr[j -1 ] > arr[j]) {
+     let temp = arr[j-1];
+     arr[j-1] = arr[j];
+     arr[j] = temp;
+     j--
+    }
+  }
+  return arr;
+}
+
+console.log(arrSort(arr));
