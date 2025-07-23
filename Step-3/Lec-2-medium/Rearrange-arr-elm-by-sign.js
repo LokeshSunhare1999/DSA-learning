544fef47195c4bb4cdd44da2b27846c34c61227a
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-07-23 23:47:27 +0530

    Rearrange array elements by sign

--- Step-3/Lec-2-medium/Rearrange-arr-elm-by-sign.js
+++ Step-3/Lec-2-medium/Rearrange-arr-elm-by-sign.js
@@ -1,14 +1,24 @@
 // Input:
-// arr[] = {1,2,-4,-5}, N = 4
+// nums[] = {1,2,-4,-5}, N = 4
 // Output: 1 -4 2 -5
 // Explanation: The array will be rearranged as 1,-4,2,-5.
 
-function rearrange(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    
+function rearrange(nums) {
+  if(nums.length == 0) return nums;
+  let result = new Array(nums.length);
+  let pos = 0;
+  let neg = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] >=0){
+      result[pos]  = nums[i];
+      pos +=2;
+    }else{
+      result[neg] = nums[i];
+      neg += 2;
+    }
   }
+  return result;
 }
 
-let arr = [1, 2, -4, -5];
-console.log(rearrange(arr))
+let nums = [1, 2, -4, -5];
+console.log(rearrange(nums))
