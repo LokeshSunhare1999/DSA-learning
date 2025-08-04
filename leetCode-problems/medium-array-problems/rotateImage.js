6fad70a584556b2c492729aa127730ae5b7baee0
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-08-04 23:34:47 +0530

    leet code question

---
+++ leetCode-problems/medium-array-problems/rotateImage.js
@@ -0,0 +1,19 @@
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  const n = matrix.length;
+  for (let i = 0; i < n; i++) { 
+    for (let j = i + 1; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse();
+  }
+  return matrix;
+};
+console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]))
