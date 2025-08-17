37ca379d9d9c764d3371ae447fddc4d562e83e10
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-08-17 17:55:36 +0530

    add two num

---
+++ leetCode-problems/medium-array-problems/addTwoNum.js
@@ -0,0 +1,38 @@
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let p = l1, q = l2, current = dummyHead;
+    let carry = 0;
+
+    while (p !== null || q !== null) {
+        const x = (p !== null) ? p.val : 0;
+        const y = (q !== null) ? q.val : 0;
+        const sum = carry + x + y;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (p !== null) p = p.next;
+        if (q !== null) q = q.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+
+    return dummyHead.next; // Return the next node of dummy head which is the actual result
+};
