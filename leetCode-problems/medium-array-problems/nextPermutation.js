8f093c57addf1fbc0e6fb7bed41821726f459633
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-08-18 22:35:33 +0530

    leet code nextPermutation

---
+++ leetCode-problems/medium-array-problems/nextPermutation.js
@@ -0,0 +1,28 @@
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+    if (i >= 0) {
+        let j = nums.length - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+    return nums;
+};
