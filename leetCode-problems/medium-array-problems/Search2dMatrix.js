a60235dacf75404a2f1f130350b1e6a46135084b
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-08-23 14:56:49 +0530

    search matrix question

---
+++ leetCode-problems/medium-array-problems/Search2dMatrix.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if (matrix.length === 0 || matrix[0].length === 0) return false;
+
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    let left = 0;
+    let right = rows * cols - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const midValue = matrix[Math.floor(mid / cols)][mid % cols];
+
+        if (midValue === target) {
+            return true;
+        } else if (midValue < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+};
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)) // true
